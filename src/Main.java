9285b65aa3e9631f42c394b251ca17882732d0da
zybert <mikolaj.sykucki@gmail.com>
2021-11-21 19:10:03 +0100

    Pierwsze zadanie

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,10 @@ public class Main {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
 
+        System.out.println("Hello world");
+        System.out.print("Hello" + " world");
+        System.out.print("\n");
+        System.out.printf("Przykład %s stringa i inta o numerze %d %s", "formatowania", 8, "i jeszcze tekst");
+
     }
 }
