6fb0a5b80669c3d9e95c5eeb182010a3b59f258b
zybert <mikolaj.sykucki@gmail.com>
2022-01-26 22:16:05 +0100

    ćw 3

--- src/Main.java
+++ src/Main.java
@@ -11,12 +11,39 @@ public class Main {
         *   dane mają być wprowadzane z klawiatury w konsoli
         *   b) wyświetlić dane za pomocą println i printf
         */
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Wprowadź imie: ");
+        String imie = scan.nextLine();
+
+        System.out.print("Wprowadź nazwisko: ");
+        String nazwisko = scan.nextLine();
+
+        System.out.print("Wprowadź wiek: ");
+        int wiek = scan.nextInt();
+
+        System.out.print("Wprowadź nr indeksu: ");
+        int nrIndexu = scan.nextInt();
+
+        System.out.printf("\n%s %s ", imie , nazwisko);
+        System.out.println("\nWiek: "+ wiek);
+        System.out.println("Nr. indeksu: "+ nrIndexu);
 
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
 
+        System.out.print("\nPodaj pierwsza liczbe: ");
+        double num1 = scan.nextInt();
+
+        System.out.print("\nPodaj druga liczbe: ");
+        double num2 = scan.nextInt();
+
+        System.out.println("Wynik dodawania: " + (num1+num2));
+        System.out.println("Wynik odejmowania: " + (num1 - num2));
+        System.out.println("Wynik mnożenia: " + num1 * num2);
+        System.out.println("Wynik dzielenia: " + num1/num2);
+        System.out.println("Wynik reszty z dzielenia: " + num1%num2);
 
     }
 }
